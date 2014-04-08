ee584e12f66a58d7579d20fb959a766258fbd045
Leonid Bushuev <leonidos@gmail.com>
2014-04-08 17:05:30 +0200

    Expose configs directory on the web using WebAccessService

--- Installer_Server/src/jetbrains/buildServer/softinstall/server/SoftInstallModelHolder.java
+++ Installer_Server/src/jetbrains/buildServer/softinstall/server/SoftInstallModelHolder.java
@@ -3,9 +3,15 @@ package jetbrains.buildServer.softinstall.server;
 import com.intellij.openapi.diagnostic.Logger;
 import jetbrains.buildServer.log.Loggers;
 import jetbrains.buildServer.serverSide.ServerPaths;
+import jetbrains.buildServer.serverSide.auth.AccessDeniedException;
+import jetbrains.buildServer.serverSide.auth.AuthorityHolder;
 import jetbrains.buildServer.softinstall.models.loader.ModelLoader;
 import jetbrains.buildServer.softinstall.models.model.SoftModel;
+import jetbrains.buildServer.web.PermissionChecker;
+import jetbrains.buildServer.web.WebAccessHelper;
+import jetbrains.buildServer.web.WebAccessService;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 
@@ -30,12 +36,50 @@ public class SoftInstallModelHolder
 
 
 
-  public SoftInstallModelHolder(@NotNull ServerPaths serverPaths)
+  public SoftInstallModelHolder(@NotNull final ServerPaths serverPaths,
+                                @NotNull final WebAccessService webAccessService)
   {
+    // READ MODEL
 
     mySoftConfigDir = new File(serverPaths.getDataDirectory(), "config/softinstall");
     readModel();
 
+    // PROVIDE ACCESS
+
+    PermissionChecker pc = new PermissionChecker()
+    {
+      @Override
+      public boolean checkPermissions(@NotNull AuthorityHolder authorityHolder)
+              throws AccessDeniedException
+      {
+        return true;
+      }
+    };
+
+    WebAccessHelper wah = new WebAccessHelper()
+    {
+      @Nullable
+      @Override
+      public String getDesiredId()
+      {
+        return "softinstall";
+      }
+
+      @Override
+      public boolean allowDownloadZip()
+      {
+        return false;
+      }
+
+      @Nullable
+      @Override
+      public String getFileNameForZip()
+      {
+        return "zip";
+      }
+    };
+
+    webAccessService.allowAccess(mySoftConfigDir, pc, wah);
   }
 
 
